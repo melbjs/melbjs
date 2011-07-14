388801187645ae1b7e1d9c543d62ee499c4ffc6e
mobz <bbirch@aconex.com>
2011-07-14 13:57:59 +1000

    fix vimeo url in article source

--- articles/2011-06-08-jquery-mobile.js
+++ articles/2011-06-08-jquery-mobile.js
@@ -7,6 +7,6 @@ module.exports = {
 		url: 'http://markdalgleish.com',
 	},
 	date: new Date(2011,5,8),
-	content: '<p>Mark shows how to get started with jQuery Mobile and introduces his custom rolled presentation framework - Fathom.js</p><p><a href="http://vimeo.com/24919623">View Video</a><br /><a href="http://markdalgleish.com/presentations/jquerymobile/">View Slides</a><br /><a href="http://markdalgleish.com/projects/fathom/">Project Homepage</a></p>',
+	content: '<p>Mark shows how to get started with jQuery Mobile and introduces his custom rolled presentation framework - Fathom.js</p><p><a href="http://vimeo.com/24918363">View Video</a><br /><a href="http://markdalgleish.com/presentations/jquerymobile/">View Slides</a><br /><a href="http://markdalgleish.com/projects/fathom/">Project Homepage</a></p>',
 
 };
