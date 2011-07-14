7ad9d295f447ab33a6e2979855e7e71c64c11fa1
Tones <tony@inlight.com.au>
2011-07-14 11:31:31 +1000

    Disabled cluster, seems to conflict with joyent's smart machine.

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
  * MelbJS Server.
  */
 
-var cluster = require('cluster');
-
 var port = process.env.PORT || 8001;
 
+var app = require('./app');
+app.listen(port);
+console.log('Server runnning on port ' + port);
+
+/*
 // Using cluster for server management.
+var cluster = require('cluster');
 cluster('./app')
 	.set('socket path', 'tmp/socks')
 	.use(cluster.logger('tmp/logs'))
@@ -16,3 +20,4 @@ cluster('./app')
 	.use(cluster.repl(8888))
 	.in('development').use(cluster.debug())
 	.listen(port);
+*/
