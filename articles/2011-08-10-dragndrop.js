5a01ba3534eed1a7f23bcff8f6394eaf50794909
Cliffano Subagio <cliffano@gmail.com>
2011-08-28 09:47:37 +1000

    Added talks from August 10th meetup.

---
+++ articles/2011-08-10-dragndrop.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	title: 'Writing games in CoffeeScript',
+	author: {
+		name: 'Ben Taylor',
+		twitterUsername: 'taybenlor',
+		url: 'http://taybenlor.com',
+	},
+	date: new Date(2011,7,10),
+	content: '<p>Ben Taylor talks about writing games in CoffeeScript, check out <a href="http://manypixies.com/">Too Many Pixies</a>.</p>',
+
+};
