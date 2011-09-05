6d5f2e32a83abe52f309b320767791fe65bed572
Benjamin Pearson <benjamin@developinginthedark.com>
2011-09-05 16:30:07 +1000

    Added slides to coffeescript talk

--- articles/2011-08-10-games-in-coffeescript.js
+++ articles/2011-08-10-games-in-coffeescript.js
@@ -7,6 +7,6 @@ module.exports = {
 		url: 'http://taybenlor.com',
 	},
 	date: new Date(2011,7,10),
-	content: '<p>Ben Taylor talks about writing games in CoffeeScript, check out <a href="http://manypixies.com/">Too Many Pixies</a>.</p><p><a href="http://vimeo.com/28491015">View Video</a></p>',
+	content: '<p>Ben Taylor talks about writing games in CoffeeScript, check out <a href="http://manypixies.com/">Too Many Pixies</a>.</p><p><a href="http://vimeo.com/28491015">View Video</a><br /><a href="http://cs-pres.taybenlor.com">View Slides</a> (Press Enter to jump between slides)</p>',
 
 };
